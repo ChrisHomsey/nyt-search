66fd19c82187623b27ccf8e95ba6dc7d4fb959ca
Chris Homsey <chomsey@Christophers-MacBook-Pro.local>
2017-11-18 14:17:47 -0500

    end of class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,8 +7,8 @@ var endDate;
 $('#search-button').click(function(){
 	searchQuery = $('#search-field').val();
 	resultNumber;
-	startDate = $('start-year-field').val() + '0101';
-	endDate = $('end-year-field').val() + '1231';
+	startDate = $('#start-year-field').val() + '0101';
+	endDate = $('#end-year-field').val() + '1231';
 	search(searchQuery, resultNumber, startDate, endDate);
 })
 
@@ -16,15 +16,17 @@ $('#clear').click(function(){
 
 })
 
-var search = function(q, num, begin_date, end_date) {
+function search(q, num, begin_date, end_date) {
 
 	var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=7dab0ac361ad41dfaec99ec48bee4a19&q=' + q + '&begin_date=' + begin_date + '&end_date=' + end_date; 
 
 	$.ajax({
 		url: queryURL,
 		method: 'GET'
-	}).done(function(repsonse){
-		console.log(result);
+	}).done(function(response){
+		console.log(response);
+
+		
 	}).fail(function(err) {
 		throw err;
 	})
